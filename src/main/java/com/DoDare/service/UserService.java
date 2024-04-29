5cffb4ef6901ef0e24af034dc123dc40e0ed27b5
Saaaaanic <you@example.com>
2024-04-29 23:55:42 +0400

    Add: basic user + register

---
+++ src/main/java/com/DoDare/service/UserService.java
@@ -0,0 +1,22 @@
+package com.DoDare.service;
+
+import com.DoDare.domain.User;
+import com.DoDare.repo.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
