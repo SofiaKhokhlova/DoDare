94b9eecdded366a03ca61f7d3af86e5bac503c78
Denys Yarkin <den.s1eap@gmail.com>
2024-05-01 23:03:00 +0200

    Basic Task table implementation w/o any relations with other tables

---
+++ src/main/java/com/DoDare/domain/Task.java
@@ -0,0 +1,29 @@
+package com.DoDare.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private Long reward;
+}
