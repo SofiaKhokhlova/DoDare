537091b2558b5da3ae35270f30547f7aecc360dd
Sofia Khokhlova <fofaanych@gmail.com>
2024-05-02 22:57:21 +0300

    Adapted pages for better display on different screen sizes

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,8 +1,24 @@
 import '../css/mainPage.css';
 import { useNavigate} from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function MainPage() {
+
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
     const nav = useNavigate();
 
     function signIn() {
@@ -26,21 +42,62 @@ function MainPage() {
     }, []);
 
     return (
-        <div id="main-page">
-            <div className="empty-space"></div>
-            <div className="header-main">
+        <div id="main-page"
+             style={{
+                 height: `${windowHeight}px`, width: `100vw`
+            }}>
+            <div className="empty-space"
+                 style={{
+                     height: `calc((32/1080) * ${windowHeight}px)`
+            }}></div>
+            <div className="header-main"
+                 style={{
+                    height: `calc((139/1080) * ${windowHeight}px)`,
+                    width: `calc((1778/1920) * 100vw)`,
+                    fontSize: `calc((30/1080) * ${windowHeight}px)`
+                }}>
+
                 <a href='' id="about" onClick={about}>About</a>
-                <div className="logo-title">
-                    <img src="/logo.png" alt="logo" />
-                    <p>DODARE</p>
+                <div className="logo-title"
+                     style={{
+                         height: `calc((139/1080) * ${windowHeight}px)`
+                    }}>
+                    <img src="/logo.png" alt="logo"
+                         style={{
+                             height: `calc((139/1080) * ${windowHeight}px)`
+                        }}/>
+                    <p
+                        style={{
+                            fontSize: `calc((50/1080) * 47.44vw)`,
+                            marginTop: `calc((36/1080) * ${windowHeight}px)`
+                    }}>
+                        DODARE</p>
                 </div>
                 <div className="buttons">
-                    <button className='sign-in' onClick={signIn}>Sign In</button>
-                    <button className='sign-up' onClick={signUp}>Sign Up</button>
+                    <button className='sign-in' onClick={signIn}
+                            style={{
+                                height: `calc((74/1080) * ${windowHeight}px)`,
+                                fontSize: `calc((30/1080) * ${windowHeight}px)`
+                    }}>
+                        Sign In</button>
+                    <button className='sign-up' onClick={signUp}
+                            style={{
+                                height: `calc((74/1080) * ${windowHeight}px)`,
+                                fontSize: `calc((30/1080) * ${windowHeight}px)`
+                    }}>
+                        Sign Up</button>
                 </div>
             </div>
-            <div className="main-info">
-                <div className="description">
+            <div className="main-info"
+                 style={{
+                     height: `calc((638/1080) * ${windowHeight}px)`,
+                     margin: `calc((54/1080) * ${windowHeight}px) auto 0 auto`
+            }}>
+                <div className="description"
+                     style={{
+                         fontSize: `calc((35/1080) * ${windowHeight}px)`,
+                         marginTop: `calc((134/1080) * ${windowHeight}px)`
+                }}>
                     <p>DoDare is a unique app that turns everyday
                         tasks into an exciting adventure. With DoDare,
                         users can create tasks based on their personal
@@ -52,10 +109,26 @@ function MainPage() {
                         and achieving new goals every day.</p>
 						<p>{message}</p>
                 </div>
-                <div className="characters">
-                    <img src="/character1.png" alt="" />
-                    <img src="/character2.png" alt="" />
-                    <img src="/character3.png" alt="" />
+                <div className="characters"
+                     style={{
+                         height: `calc((261/1080) * ${windowHeight}px)`,
+                         marginTop: `calc((160/1080) * ${windowHeight}px)`
+                }}>
+                    <img src="/character1.png" alt=""
+                         style={{
+                             height: `calc((229/1080) * ${windowHeight}px)`,
+                             width: `calc((169/1920) * 100vw)`
+                    }}/>
+                    <img src="/character2.png" alt=""
+                         style={{
+                             height: `calc((261/1080) * ${windowHeight}px)`,
+                             width: `calc((200/1920) * 100vw)`
+                        }}/>
+                    <img src="/character3.png" alt=""
+                         style={{
+                             height: `calc((247/1080) * ${windowHeight}px)`,
+                             width: `calc((133/1920) * 100vw)`
+                        }}/>
                 </div>
             </div>
         </div>
--- frontend/src/components/MyTasks.tsx
+++ frontend/src/components/MyTasks.tsx
@@ -1,7 +1,21 @@
 import '../css/myTasks.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function MyTasks() {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     type TaskList = {
         id: number;
         title: string;
@@ -107,68 +121,211 @@ function MyTasks() {
 
     return (
         <>
-            <div className="my-tasks-page">
-                <header>
-                    <div className="logo-title-container">
-                        <img src="/logo.png" alt="logo" />
-                        <p>DODARE</p>
+            <div className="my-tasks-page"
+                 style={{
+                     height: `${windowHeight}px`
+                 }}>
+                <header
+                    style={{
+                        height: `calc((91/1080) * ${windowHeight}px)`
+                }}>
+                    <div className="logo-title-container" >
+                        <img src="/logo.png" alt="logo"
+                             style={{
+                                 height: `calc((94/1080) * ${windowHeight}px)`
+                        }} />
+                        <p
+                            style={{
+                                fontSize: `calc((50/1080) * ${windowHeight}px`,
+                                margin: `calc((15/1080) * ${windowHeight}px) 0 0 0`
+                        }}>
+                            DODARE</p>
                     </div>
-                    <div className="profile-header">
-                        <img src="/point.png" alt="" />
+                    <div className="profile-header"
+                         style={{
+                             fontSize: `calc((25/1080) * ${windowHeight}px`
+                         }}>
+                        <img src="/point.png" alt=""
+                             style={{
+                                 height: `calc((34/1080) * ${windowHeight}px)`
+                        }} />
                         <p className='point'>0</p>
                         <p className='user'>user_name</p>
-                        <a className='profile-photo' href=""></a>
+                        <a className='profile-photo' href=""
+                           style={{
+                               height: `calc((60/1080) * ${windowHeight}px)`
+                        }}></a>
                     </div>
                 </header>
-                <div className="sidebar">
-                    <div className="menu">
-                        <div className="my-tasks">
-                            <img src="/mytask-icon.png" alt="mytask" />
-                            <a href="">My tasks</a>
+                <div className="sidebar"
+                     style={{
+                         marginTop:`calc((91/1080) * ${windowHeight}px)`,
+                         height:`calc((989/1080) * ${windowHeight}px)`
+                     }}>
+                    <div className="menu"
+                         style={{
+                             marginTop:`calc((58/1080) * ${windowHeight}px)`,
+                             fontSize:`calc((35/1080) * ${windowHeight}px)`
+                         }}>
+                        <div className="my-tasks"
+                             style={{
+                                 height:`calc((51/1080) * ${windowHeight}px)`,
+                                 marginBottom:`calc((10/1080) * ${windowHeight}px)`
+                        }}>
+                            <img src="/mytask-icon.png" alt="mytask"
+                                 style={{
+                                     height:`calc((35/1080) * ${windowHeight}px)`,
+                                     margin: `calc((9/1080) * ${windowHeight}px) 0 0 calc((15/1920) * 100vw)`
+                                 }} />
+                            <a href=""
+                               style={{
+                                   margin: `calc((6/1080) * ${windowHeight}px) 0 0 calc((23/1920) * 100vw)`
+                                }}>
+                                My tasks</a>
                         </div>
-                        <div className="groups">
-                            <img src="/groups-icon.png" alt="groups" />
-                            <a href="">Groups</a>
+                        <div className="groups"
+                             style={{
+                                 height:`calc((55/1080) * ${windowHeight}px)`,
+                                 marginBottom:`calc((10/1080) * ${windowHeight}px)`
+                             }}>
+                            <img src="/groups-icon.png" alt="groups"
+                                 style={{
+                                     height:`calc((35/1080) * ${windowHeight}px)`,
+                                     margin: `calc((9/1080) * ${windowHeight}px) 0 0 calc((15/1920) * 100vw)`
+                            }} />
+                            <a href=""
+                               style={{
+                                   margin: `calc((6/1080) * ${windowHeight}px) 0 0 calc((23/1920) * 100vw)`
+                            }}>
+                                Groups</a>
                         </div>
-                        <div className="store">
-                            <img src="/store-icon.png" alt="store" />
-                            <a href="">Store</a>
+                        <div className="store"
+                             style={{
+                                 height:`calc((55/1080) * ${windowHeight}px)`,
+                                 marginBottom:`calc((10/1080) * ${windowHeight}px)`
+                        }}>
+                            <img src="/store-icon.png" alt="store"
+                                 style={{
+                                     height:`calc((35/1080) * ${windowHeight}px)`,
+                                     margin: `calc((9/1080) * ${windowHeight}px) 0 0 calc((15/1920) * 100vw)`
+                            }} />
+                            <a href=""
+                               style={{
+                                   margin: `calc((6/1080) * ${windowHeight}px) 0 0 calc((23/1920) * 100vw)`
+                            }}>
+                                Store</a>
                         </div>
-                        <div className="stats">
-                            <img src="/stats-icon.png" alt="stats" />
-                            <a href="">Statistics</a>
+                        <div className="stats"
+                             style={{
+                                 height:`calc((55/1080) * ${windowHeight}px)`,
+                                 marginBottom:`calc((10/1080) * ${windowHeight}px)`
+                        }}>
+                            <img src="/stats-icon.png" alt="stats"
+                                 style={{
+                                     height:`calc((35/1080) * ${windowHeight}px)`,
+                                     margin: `calc((9/1080) * ${windowHeight}px) 0 0 calc((15/1920) * 100vw)`
+                            }} />
+                            <a href=""
+                               style={{
+                                   margin: `calc((6/1080) * ${windowHeight}px) 0 0 calc((23/1920) * 100vw)`
+                            }}>
+                                Statistics</a>
                         </div>
-                        <div className="inventory">
-                            <img src="/inventory-icon.png" alt="inventory" />
-                            <a href="">Inventory</a>
+                        <div className="inventory"
+                             style={{
+                                 height:`calc((55/1080) * ${windowHeight}px)`,
+                                 marginBottom:`calc((10/1080) * ${windowHeight}px)`
+                             }}>
+                            <img src="/inventory-icon.png" alt="inventory"
+                                 style={{
+                                     height:`calc((35/1080) * ${windowHeight}px)`,
+                                     margin: `calc((9/1080) * ${windowHeight}px) 0 0 calc((15/1920) * 100vw)`
+                            }} />
+                            <a href=""
+                               style={{
+                                   margin: `calc((6/1080) * ${windowHeight}px) 0 0 calc((23/1920) * 100vw)`
+                            }}>
+                                Inventory</a>
                         </div>
                     </div>
-                    <div className="log-out">
-                        <img src="/exit-icon.png" alt="log-out" />
-                        <a href="">Log out</a>
+                    <div className="log-out"
+                         style={{
+                             bottom:`calc((66/1080) * ${windowHeight}px)`,
+                             height:`calc((55/1080) * ${windowHeight}px)`
+                         }}>
+                        <img src="/exit-icon.png" alt="log-out"
+                             style={{
+                                 height:`calc((35/1080) * ${windowHeight}px)`
+                             }} />
+                        <a href=""
+                           style={{
+                               fontSize:`calc((35/1080) * ${windowHeight}px)`
+                        }}>
+                            Log out</a>
                     </div>
                 </div>
 
-                <div className="tasks-container">
-                    <p className='your-tasks'>Your tasks</p>
-                    <div className="task-overflow">
+                <div className="tasks-container"
+                     style={{
+                         height:`calc((712/1080) * ${windowHeight}px)`,
+                         margin:`calc((150/1080) * ${windowHeight}px) 0 0 calc((114/1920) * 100vw)`
+                     }}>
+                    <p className='your-tasks'
+                       style={{
+                           fontSize:`calc((50/1080) * ${windowHeight}px)`,
+                           marginBottom:`calc((32/1080) * ${windowHeight}px)`
+                        }}>
+                        Your tasks</p>
+                    <div className="task-overflow"
+                         style={{
+                             height:`calc((712/1080) * ${windowHeight}px)`
+                         }}>
                         {tasks.map((task) => (
-                            <div key={task.id} className="task">
+                            <div key={task.id} className="task"
+                                 style={{
+                                     height:`calc((64/1080) * ${windowHeight}px)`,
+                                     marginBottom:`calc((17/1080) * ${windowHeight}px)`
+                                 }}>
                                 <div className="title-deadline">
-                                    <p className="task-title">{task.title}</p>
-                                    <p className="task-deadline">available till: {task.deadline}</p>
+                                    <p className="task-title"
+                                       style={{
+                                           height:`calc((25/1080) * ${windowHeight}px)`,
+                                           fontSize:`calc((22/1080) * ${windowHeight}px)`,
+                                           margin:`calc((8/1080) * ${windowHeight}px) 0 0 calc((19/1920) * 100vw)`
+                                    }}>
+                                        {task.title}</p>
+                                    <p className="task-deadline"
+                                       style={{
+                                           fontSize:`calc((14/1080) * ${windowHeight}px)`,
+                                           margin:`calc((7/1080) * ${windowHeight}px) 0 calc((8/1080) * ${windowHeight}px) calc((20/1920) * 100vw)`,
+                                           height:`calc((16/1080) * ${windowHeight}px)`
+                                    }}>
+                                        available till: {task.deadline}</p>
                                 </div>
                                 <div className="task-content">
-                                    <img src="/point.png" alt="point-img" />
-                                    <p className="task-reward">{task.reward}</p>
+                                    <img src="/point.png" alt="point-img"
+                                         style={{
+                                             height:`calc((27/1080) * ${windowHeight}px)`
+                                    }} />
+                                    <p className="task-reward"
+                                       style={{
+                                           fontSize:`calc((25/1080) * ${windowHeight}px)`
+                                    }}>
+                                        {task.reward}</p>
                                     <input
+                                        style={{
+                                            height:`calc((33/1080) * ${windowHeight}px)`
+                                        }}
                                         id="taskCheckbox"
                                         className='status'
                                         type="checkbox"
                                         checked={task.status === 'done'}
                                         onChange={() => handleTaskStatus(task.id)}
                                     />
-                                    <label htmlFor="taskCheckbox"></label>
+                                    <label htmlFor="taskCheckbox"
+                                           style={{
+                                               height:`calc((25/1080) * ${windowHeight}px)`
+                                    }}></label>
                                 </div>
                             </div>
                         ))}
