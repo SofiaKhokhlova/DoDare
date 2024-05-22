67e3a880bb6e0a75648f04a2d3de0bd91e214b90
Denys Yarkin <den.s1eap@gmail.com>
2024-05-22 22:47:38 +0200

    Added endpoint for getting information about the account for authorized user

--- src/main/java/com/DoDare/controller/UserController.java
+++ src/main/java/com/DoDare/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.DoDare.controller;
 
+import com.DoDare.dto.UserDTO;
 import com.DoDare.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -14,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getUser(
+            @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        return ResponseEntity.ok(userService.findByEmail(username));
+    }
 
     // for testing purposes only
     @PostMapping("/addPoints")
